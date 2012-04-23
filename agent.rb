3a47d363e9ffb5d8b411c72965cdea7e4e89b48b
Travis Hall <trvs.hll@gmail.com>
2012-04-23 10:14:35 -0700

    Added CLI to agent.rb (no batching)

--- agent.rb
+++ agent.rb
@@ -1,6 +1,8 @@
 #!/usr/bin/env ruby
 
+require 'optparse'
 require 'gruff'
+require 'yaml'
 
 class Food
   attr_reader :score, :health
@@ -43,19 +45,27 @@ end
 class Agent
 
   WELLS = {
-    :unhealthy => Food.new(10, -6),
-    :healthy   => Food.new(6, -1)
+    :unhealthy => Food.new(10, -5),
+    :healthy   => Food.new(6, 0)
   }
 
   attr_reader :run_number, :days_lived, :history, :health, :qvalue, :history
-  def initialize(checkup_frequency, exploration, epsilon, alpha, gamma, health = 100, retain=false)
-    @retain_qvalues = retain
-    @epsilon = epsilon
-    @alpha = alpha
-    @gamma = gamma
-    @exploration = exploration
-    @checkup_frequency = checkup_frequency
-    @health_max = health
+
+  def initialize(options)
+    @options = options # maintain for "thinking"
+
+    @retain_qvalues = options[:retain]
+    @epsilon = options[:epsilon]
+    @alpha = options[:alpha]
+    @gamma = options[:gamma]
+    @exploration = options[:exploration]
+    @checkup_frequency = options[:checkup]
+    @thinking_frequency = options[:think]
+    @health_max = options[:health]
+
+    @history_file = options[:history]
+    @graph_file = options[:graph]
+
     @run_number = 0
 
     graph_setup
@@ -98,6 +108,8 @@ class Agent
 
     assess_lifespan
     add_to_graph
+
+    @history_file.puts @history.inspect unless @history_file.nil?
   end
 
   def checkup_time
@@ -170,19 +182,109 @@ class Agent
   def save_graph
     @graph_q.labels = { 0 => '0',
                         @graph_max-1 => "#{@graph_max-1}",
-                        @exploration => "#{@exploration}",
-                        @checkup_frequency => "#{@checkup_frequency}"}
-    @graph_q.write('q-values.png')
+                        @exploration => "Explore: #{@exploration}",
+                        @checkup_frequency => "Checkup: #{@checkup_frequency}"}
+    @graph_q.write(@graph_file)
+  end
+
+  # TODO: add food settings
+  def to_yaml
+    OPTIONS.merge(WELLS).to_yaml
   end
 end
 
-runs = 2
+OPTIONS = {
+  ## Batch config file
+  :config => nil,
 
-history_file = File.open("history.txt", "w")
-# checkup frequency, exploration, epsilon, alpha, gamma, health, retain memory
-agent = Agent.new(1, 0, 15, 0.2, 0.8, 2000, false)
-runs.times do
+  ## Agent settings
+  :checkup => 10,
+  :think => 10,
+  :exploration => 50,
+  :epsilon => 15,
+  :alpha => 0.2,
+  :gamma => 0.8,
+  :health => 100,
+  :retain => false,
+  :history => nil,
+  :graph => "q-values.png",
+
+  ## Script settings
+  :runs => 1,
+  :save => nil
+}
+
+OptionParser.new do |opts|
+  opts.banner = "Usage: #{$0} [options]"
+
+  opts.on("--config [FILE]", "Configuration file for batched experiments") do |f|
+    OPTIONS[:config] = f
+  end
+
+  opts.on("-c", "--checkup [NUM]", Integer, "Checkup frequency",
+          "   Default: #{OPTIONS[:checkup]}") do |i|
+    OPTIONS[:checkup] = i
+  end
+
+  opts.on("-t", "--think [NUM]", Integer, "Thinking frequency (in number of checkups)",
+          "   Default: #{OPTIONS[:think]}") do |i|
+    OPTIONS[:think] = i
+  end
+
+  opts.on("-e", "--exploration [NUM]", Integer, "Number of exploration stage choices",
+          "   Default: #{OPTIONS[:exploration]}") do |i|
+    OPTIONS[:exploration] = i
+  end
+
+  opts.on("-p", "--epsilon [NUM]", Integer, "Greedy epsilon value (integer [15 = 15%])",
+          "   Default: #{OPTIONS[:epsilon]}") do |i|
+    OPTIONS[:epsilon] = i
+  end
+
+  opts.on("-a", "--alpha [FLOAT]", Float, "Alpha value for reinforcement learning",
+          "   Default: #{OPTIONS[:alpha]}") do |f|
+    OPTIONS[:alpha] = f
+  end
+
+  opts.on("-g", "--gamma [FLOAT]", Float, "Gamma value for reinforcement learning",
+          "   Default: #{OPTIONS[:gamma]}") do |f|
+    OPTIONS[:gamma] = f
+  end
+
+  opts.on("-h", "--health [NUM]", Integer, "Health value for the agent",
+          "   Default: #{OPTIONS[:health]}") do |i|
+    OPTIONS[:health] = i
+  end
+
+  opts.on("-r", "--retain", "Retain Q-Values between runs", "   Default: #{OPTIONS[:retain]}") do
+    OPTIONS[:retain] = true
+  end
+
+  opts.on("-y", "--history [FILE]", "History log file. If none supplied, will not log") do |f|
+    OPTIONS[:history] = File.open(f, "w")
+  end
+
+  opts.on("-q", "--graph [FILE]", "Q-learning graph output file", "   Default: #{OPTIONS[:graph]}") do |f|
+    OPTIONS[:graph] = f
+  end
+
+  opts.on("-u", "--runs [NUM]", Integer, "Number of runs for an agent",
+          "   Default: #{OPTIONS[:runs]}") do |i|
+    OPTIONS[:runs] = i
+  end
+
+  opts.on("--save [FILE]", "Save options configuration to file. If none supplied, will not save") do |f|
+    $save = File.open(f, "w")
+  end
+end.parse!
+
+agent = Agent.new(OPTIONS)
+OPTIONS[:runs].times do
   agent.run()
-  history_file.puts agent.history.inspect
 end
 agent.save_graph
+
+unless $save.nil?
+  $save.write agent.to_yaml
+  $save.close
+end
